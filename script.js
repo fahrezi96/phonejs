b3c94bfe0da2b18aa4a4b065ac72df2ed79ff246
Muhamad Irgi fahrezi <muhamad.irgi@indocyber.id>
2025-02-01 09:29:14 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const phoneLabel = document.getElementById("label");
+const phoneInput = document.getElementById("phone");
+const getPhoneBtn = document.getElementById("getphone");
+
+getPhoneBtn.addEventListener("click", getContact);
+
+async function getContact() {
+  if (!("contacts" in navigator && "ContactsManager" in window)) {
+    alert("Contacts API not supported on this browser.");
+    return;
+  }
+
+  try {
+    const props = ["name", "tel"];
+    const opts = { multiple: false };
+    const contacts = await navigator.contacts.select(props, opts);
+
+    if (contacts.length > 0) {
+      phoneInput.value = contacts[0].tel;
+      phoneLabel.textContent = contacts[0].name;
+    }
+  } catch (error) {
+    console.error("Error fetching contact:", error);
+  }
+}
